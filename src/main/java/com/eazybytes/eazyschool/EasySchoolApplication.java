63133188bf8e5795cdca68657e7327fd8229066d
MOSTAFA\hasna <mostafa.mm661@gmail.com>
2025-07-13 18:33:05 +0300

    fix project name

--- src/main/java/com/eazybytes/eazyschool/EasySchoolApplication.java
+++ src/main/java/com/eazybytes/eazyschool/EasySchoolApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class EazyschoolApplication {
+public class EasySchoolApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(EazyschoolApplication.class, args);
+		SpringApplication.run(EasySchoolApplication.class, args);
 	}
 
 }
